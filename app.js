7cac2263631c58d5fd0aa3bf414cb2bb6f61dc37
Matthew T Geiger <matthewgeiger99@gmail.com>
2019-01-08 03:19:51 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 // Listen to port
 app.listen(3000, function(){
     console.log("Listening on port 3000");
+    setInterval(function(){
+        console.log("Server Status: stable");
+    }, 60000);
 });
 
 // Root query
